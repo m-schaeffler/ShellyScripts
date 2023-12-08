fbaef5327945b017b554ed70246f028a6a981c42
Mathias Schäffler <mathias@fam-schaeffler.de>
2023-12-08 08:22:58 +0100

    aktueller Stand

--- KeepAlive.js
+++ KeepAlive.js
@@ -2,28 +2,30 @@ let mqttPrefix = Shelly.getComponentConfig( "mqtt" ).topic_prefix;
 let index      = 0;
 let component  = "switch";
 
-Timer.set( 60*1000, true,
+Timer.set( 90*1000, true,
     function(ud)
     {
         let result = Shelly.getComponentStatus( component, index );
-        MQTT.publish( mqttPrefix+"/status/"+component+":"+JSON.stringify(index), 
-                      JSON.stringify( result ), 0, false );
-        if( index < 2 )
+        let topic  = mqttPrefix+"/status/"+component+":"+JSON.stringify(index);
+        if( component === "switch" )
         {
-            index++;
+            delete result.source;
+            delete result.temperature.tF;
+            component = "input";
         }
         else
         {
-            index = 0;
-            if( component === "switch" )
+            component = "switch";
+            if( index < 2 )
             {
-                component = "input";
+                index++;
             }
             else
             {
-                component = "switch";
+                index = 0;
             }
         }
+        MQTT.publish( topic, JSON.stringify( result ), 0, false );
     },
     null
 );
